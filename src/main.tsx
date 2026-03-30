22ab26804b4fc139ce8755b4ae5b8783fc6cc7f2
rituparna <rituparna.pal@weavers-web.com>
2026-03-30 19:17:37 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
+import './App.css'
+import './index.css'
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
